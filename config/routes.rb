411773723fc411768b1df744903ff1adb821f359
joelgroomer <joelgroomer@users.noreply.github.com>
2023-10-01 18:08:18 -0700

    Make static_pages#landing_page the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
+  root "static_pages#landing_page"
   get 'static_pages/privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
